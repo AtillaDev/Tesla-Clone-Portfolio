fef9a07e6b976ee990bfe9fe76a8c7f59e70011d
Atila <atiladev@abv.bg>
2022-12-14 17:28:25 +0200

    fixed scrollbar bugt

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,7 @@ const navigation = document.getElementById("nav");
 const blurOverlay = document.getElementById("blur");
 const menuBtn = document.getElementById("menuBtn");
 const footer = document.getElementById("footer");
+const secondSection = document.getElementById("section-two");
 
 new fullpage("#fullpage", {
   autoScrolling: true,
@@ -11,15 +12,23 @@ new fullpage("#fullpage", {
   scrollingSpeed: 400,
 });
 
+// $("#fullpage").fullpage({
+//   anchors: ["section1", "section2", "section3", "section4"],
+// });
+
 const toggleNavigation = () => {
   navigation.classList.toggle("is--active");
   blurOverlay.classList.toggle("is--active");
-  fullpageEl.classList.toggle("no-scroll");
+  // fullpageEl.classList.toggle("no-scroll");
 };
 
 menuBtn.addEventListener("click", toggleNavigation);
 closeBtn.addEventListener("click", toggleNavigation);
 blurOverlay.addEventListener("click", toggleNavigation);
+
+// closeBtn.addEventListener("click", () => {
+//   secondSection.classList.add("active");
+// });
 
 // $("#fullpage").fullpage({
 //   onLeave: function (index, nextIndex, direction) {
